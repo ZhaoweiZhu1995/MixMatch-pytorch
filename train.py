9678f9a087098fe4e6d23c36cdbac2d411399399
ZhaoweiZhu1995 <zwzhu1995@gmail.com>
2021-02-20 15:45:15 -0800

    Update train.py

--- train.py
+++ train.py
@@ -22,35 +22,7 @@ import dataset.cifar10 as dataset
 from utils import Bar, Logger, AverageMeter, accuracy, mkdir_p, savefig, AverageMeterVector
 from tensorboardX import SummaryWriter
 
-parser = argparse.ArgumentParser(description='PyTorch MixMatch Training')
-# Optimization options
-parser.add_argument('--epochs', default=1024, type=int, metavar='N',
-                    help='number of total epochs to run')
-parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
-                    help='manual epoch number (useful on restarts)')
-parser.add_argument('--batch-size', default=64, type=int, metavar='N',
-                    help='train batchsize')
-parser.add_argument('--lr', '--learning-rate', default=0.002, type=float,
-                    metavar='LR', help='initial learning rate')
-# Checkpoints
-parser.add_argument('--resume', default='', type=str, metavar='PATH',
-                    help='path to latest checkpoint (default: none)')
-# Miscs
-parser.add_argument('--manualSeed', type=int, default=0, help='manual seed')
-#Device options
-parser.add_argument('--gpu', default='0', type=str,
-                    help='id(s) for CUDA_VISIBLE_DEVICES')
-#Method options
-parser.add_argument('--n-labeled', type=int, default=250,
-                        help='Number of labeled data')
-parser.add_argument('--train-iteration', type=int, default=1024,
-                        help='Number of iteration per epoch')
-parser.add_argument('--out', default='result',
-                        help='Directory to output the result')
-parser.add_argument('--alpha', default=0.75, type=float)
-parser.add_argument('--lambda-u', default=75, type=float)
-parser.add_argument('--T', default=0.5, type=float)
-parser.add_argument('--ema-decay', default=0.999, type=float)
+
 
 
 
@@ -406,6 +378,35 @@ def interleave(xy, batch):
     return [torch.cat(v, dim=0) for v in xy]
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='PyTorch MixMatch Training')
+    # Optimization options
+    parser.add_argument('--epochs', default=1024, type=int, metavar='N',
+                        help='number of total epochs to run')
+    parser.add_argument('--start-epoch', default=0, type=int, metavar='N',
+                        help='manual epoch number (useful on restarts)')
+    parser.add_argument('--batch-size', default=64, type=int, metavar='N',
+                        help='train batchsize')
+    parser.add_argument('--lr', '--learning-rate', default=0.002, type=float,
+                        metavar='LR', help='initial learning rate')
+    # Checkpoints
+    parser.add_argument('--resume', default='', type=str, metavar='PATH',
+                        help='path to latest checkpoint (default: none)')
+    # Miscs
+    parser.add_argument('--manualSeed', type=int, default=0, help='manual seed')
+    #Device options
+    parser.add_argument('--gpu', default='0', type=str,
+                        help='id(s) for CUDA_VISIBLE_DEVICES')
+    #Method options
+    parser.add_argument('--n-labeled', type=int, default=250,
+                            help='Number of labeled data')
+    parser.add_argument('--train-iteration', type=int, default=1024,
+                            help='Number of iteration per epoch')
+    parser.add_argument('--out', default='result',
+                            help='Directory to output the result')
+    parser.add_argument('--alpha', default=0.75, type=float)
+    parser.add_argument('--lambda-u', default=75, type=float)
+    parser.add_argument('--T', default=0.5, type=float)
+    parser.add_argument('--ema-decay', default=0.999, type=float)
     print('Runing here', flush = True)
     
     args = parser.parse_args()
@@ -421,4 +422,4 @@ if __name__ == '__main__':
     np.random.seed(args.manualSeed)
 
     best_acc = 0  # best test accuracy
-    main()
+    # main()
