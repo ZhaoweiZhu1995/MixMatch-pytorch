23dcb3705941d680f0fd14102a9f30ef66ec7c7e
ZhaoweiZhu1995 <zwzhu1995@gmail.com>
2021-02-25 13:20:45 -0800

    Update train.py

--- train.py
+++ train.py
@@ -304,51 +304,169 @@ def train(labeled_trainloader, unlabeled_trainloader, model, optimizer, ema_opti
 
     return (losses.avg, losses_x.avg, losses_u.avg,)
 
-def train_small(labeled_trainloader, model, optimizer, criterion, epoch, use_cuda, num_classes = 10):
+def train_small(labeled_trainloader, unlabeled_trainloader, model, optimizer, ema_optimizer, criterion, epoch, use_cuda):
 
     batch_time = AverageMeter()
     data_time = AverageMeter()
     losses = AverageMeter()
-    top1 = AverageMeter()
-    top5 = AverageMeter()
-    top1_per_class = AverageMeterVector(num_classes)
-
-    # switch to evaluate mode
-    model.train()
-
+    losses_x = AverageMeter()
+    losses_u = AverageMeter()
+    ws = AverageMeter()
     end = time.time()
-    # bar = Bar(f'{mode}', max=len(labeled_trainloader))
 
-    for batch_idx, (inputs, targets) in enumerate(labeled_trainloader):
+    bar = Bar('Training', max=args.train_iteration)
+    labeled_train_iter = iter(labeled_trainloader)
+    unlabeled_train_iter = iter(unlabeled_trainloader)
+
+    model.train()
+    for batch_idx in range(args.train_iteration):
+        try:
+            inputs_x, targets_x = labeled_train_iter.next()
+        except:
+            labeled_train_iter = iter(labeled_trainloader)
+            inputs_x, targets_x = labeled_train_iter.next()
+
+        try:
+            (inputs_u, inputs_u2), _ = unlabeled_train_iter.next()
+        except:
+            unlabeled_train_iter = iter(unlabeled_trainloader)
+            (inputs_u, inputs_u2), _ = unlabeled_train_iter.next()
+
         # measure data loading time
         data_time.update(time.time() - end)
 
+        batch_size = inputs_x.size(0)
+
+        # Transform label to one-hot
+        targets_x = torch.zeros(batch_size, 10).scatter_(1, targets_x.view(-1,1).long(), 1)
+
         if use_cuda:
-            inputs, targets = inputs.cuda(), targets.cuda(non_blocking=True)
-        # compute output
-        outputs = model(inputs)
-        loss = criterion(outputs, targets)
+            inputs_x, targets_x = inputs_x.cuda(), targets_x.cuda(non_blocking=True)
+            inputs_u = inputs_u.cuda()
+            inputs_u2 = inputs_u2.cuda()
 
-        # measure accuracy and record loss
-        prec1, prec5 = accuracy(outputs, targets, topk=(1, 5))
-        prec1_per_class, rec_num = accuracy(outputs, targets, topk=(1,), per_class=True)
-        # losses.update(loss.item(), inputs.size(0))
-        top1_per_class.update(prec1_per_class.cpu().numpy(), rec_num.cpu().numpy())
 
-        top1.update(prec1.item(), inputs.size(0))
-        top5.update(prec5.item(), inputs.size(0))
+        with torch.no_grad():
+            # compute guessed labels of unlabel samples
+            outputs_u = model(inputs_u)
+            outputs_u2 = model(inputs_u2)
+            p = (torch.softmax(outputs_u, dim=1) + torch.softmax(outputs_u2, dim=1)) / 2
+            pt = p**(1/args.T)
+            targets_u = pt / pt.sum(dim=1, keepdim=True)
+            targets_u = targets_u.detach()
 
-        # measure elapsed time
-        batch_time.update(time.time() - end)
-        end = time.time()
+        # mixup
+        all_inputs = torch.cat([inputs_x, inputs_u, inputs_u2], dim=0)
+        all_targets = torch.cat([targets_x, targets_u, targets_u], dim=0)
+
+        l = np.random.beta(args.alpha, args.alpha)
+
+        l = max(l, 1-l)
+
+        idx = torch.randperm(all_inputs.size(0))
+
+        input_a, input_b = all_inputs, all_inputs[idx]
+        target_a, target_b = all_targets, all_targets[idx]
+
+        mixed_input = l * input_a + (1 - l) * input_b
+        mixed_target = l * target_a + (1 - l) * target_b
+
+        # interleave labeled and unlabed samples between batches to get correct batchnorm calculation 
+        mixed_input = list(torch.split(mixed_input, batch_size))
+        mixed_input = interleave(mixed_input, batch_size)
+
+        logits = [model(mixed_input[0])]
+        for input in mixed_input[1:]:
+            logits.append(model(input))
+
+        # put interleaved samples back
+        logits = interleave(logits, batch_size)
+        logits_x = logits[0]
+        logits_u = torch.cat(logits[1:], dim=0)
+
+        Lx, Lu, w = criterion(logits_x, mixed_target[:batch_size], logits_u, mixed_target[batch_size:], epoch+batch_idx/args.train_iteration)
+
+        loss = Lx + w * Lu
+
+        # record loss
+        losses.update(loss.item(), inputs_x.size(0))
+        losses_x.update(Lx.item(), inputs_x.size(0))
+        losses_u.update(Lu.item(), inputs_x.size(0))
+        ws.update(w, inputs_x.size(0))
 
         # compute gradient and do SGD step
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
+        ema_optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+
+        # plot progress
+        bar.suffix  = '({batch}/{size}) Data: {data:.3f}s | Batch: {bt:.3f}s | Total: {total:} | ETA: {eta:} | Loss: {loss:.4f} | Loss_x: {loss_x:.4f} | Loss_u: {loss_u:.4f} | W: {w:.4f}'.format(
+                    batch=batch_idx + 1,
+                    size=args.train_iteration,
+                    data=data_time.avg,
+                    bt=batch_time.avg,
+                    total=bar.elapsed_td,
+                    eta=bar.eta_td,
+                    loss=losses.avg,
+                    loss_x=losses_x.avg,
+                    loss_u=losses_u.avg,
+                    w=ws.avg,
+                    )
+        bar.next()
+    bar.finish()
+
+    return (losses.avg, losses_x.avg, losses_u.avg,)
+
+# def train_small(labeled_trainloader, model, optimizer, criterion, epoch, use_cuda, num_classes = 10):
+
+#     batch_time = AverageMeter()
+#     data_time = AverageMeter()
+#     losses = AverageMeter()
+#     top1 = AverageMeter()
+#     top5 = AverageMeter()
+#     top1_per_class = AverageMeterVector(num_classes)
+
+#     # switch to evaluate mode
+#     model.train()
+
+#     end = time.time()
+#     # bar = Bar(f'{mode}', max=len(labeled_trainloader))
+
+#     for batch_idx, (inputs, targets) in enumerate(labeled_trainloader):
+#         # measure data loading time
+#         data_time.update(time.time() - end)
+
+#         if use_cuda:
+#             inputs, targets = inputs.cuda(), targets.cuda(non_blocking=True)
+#         # compute output
+#         outputs = model(inputs)
+#         loss = criterion(outputs, targets)
+
+#         # measure accuracy and record loss
+#         prec1, prec5 = accuracy(outputs, targets, topk=(1, 5))
+#         prec1_per_class, rec_num = accuracy(outputs, targets, topk=(1,), per_class=True)
+#         # losses.update(loss.item(), inputs.size(0))
+#         top1_per_class.update(prec1_per_class.cpu().numpy(), rec_num.cpu().numpy())
+
+#         top1.update(prec1.item(), inputs.size(0))
+#         top5.update(prec5.item(), inputs.size(0))
+
+#         # measure elapsed time
+#         batch_time.update(time.time() - end)
+#         end = time.time()
+
+#         # compute gradient and do SGD step
+#         optimizer.zero_grad()
+#         loss.backward()
+#         optimizer.step()
 
 
-    print(f'[*train_small] top1_per_class accuracy is: {np.round(top1_per_class.avg,2)}, average: {np.round(top1.avg,4)}', flush = True)
+#     print(f'[*train_small] top1_per_class accuracy is: {np.round(top1_per_class.avg,2)}, average: {np.round(top1.avg,4)}', flush = True)
         
     # return (losses.avg, top1.avg)
 
